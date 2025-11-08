5b25469167a99f007bf7311264e0f9783e31f771
saiteja <saiteja@hebbale.academy>
2025-11-08 08:04:16 +0000

    completed the task-1 and task-2

--- assignment_1/Task_1.py
+++ assignment_1/Task_1.py
@@ -1,6 +1,3 @@
-# python-
-
-Task-1
 """
 Problem Statement: Write a Python program that does the following:
 1.  Takes two numbers as input from the user.
@@ -26,20 +23,3 @@ print("Addition : ", addition)
 print("Subtraction : ", subtraction)
 print("Multiplication : ", multiplication)
 print("Division : ", division)
-
-Task-2
-
-""" "
-Problem Statement: Write a Python program that:
-1.  Takes a user's first name and last name as input.
-2.  Concatenates the first name and last name into a full name.
-3.  Prints a personalized greeting message using the full name.
-
-"""
-
-first_name = input("Enter the first name : ")
-last_name = input("Enter the last name : ")
-
-greeting = "Hello, " + first_name + " " + last_name + "! Welcome to the Python program."
-
-print(greeting)
---
+++ assignment_1/Task_2.py
@@ -0,0 +1,14 @@
+""" "
+Problem Statement: Write a Python program that:
+1.  Takes a user's first name and last name as input.
+2.  Concatenates the first name and last name into a full name.
+3.  Prints a personalized greeting message using the full name.
+
+"""
+
+first_name = input("Enter the first name : ")
+last_name = input("Enter the last name : ")
+
+greeting = "Hello, " + first_name + " " + last_name + "! Welcome to the Python program."
+
+print(greeting)
