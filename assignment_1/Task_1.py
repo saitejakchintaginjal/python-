94688716910c3e69bd573410f1842fbc64553f54
saitejakchintaginjal <saitejakchintaginjal@gmail.com>
2025-11-10 04:53:18 +0000

    testing

--- assignment_1/Task_1.py
+++ assignment_1/Task_1.py
@@ -10,8 +10,8 @@ o	Division
 
 """
 
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
+num1 = int(input("Enter the first number:  "))
+num2 = int(input("Enter the second number:  "))
 
 addition = num1 + num2
 subtraction = num1 - num2
